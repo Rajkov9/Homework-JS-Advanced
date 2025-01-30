98e4b9468607abc955087a1ab3cc1418a16dcf44
Rajkov9 <rajkov.ivan24@gmail.com>
2025-01-30 17:04:34 +0100

    modifications

--- Homework 2/Task 3/part3.js
+++ Homework 2/Task 3/part3.js
@@ -1,14 +1,16 @@
 let button = document.getElementById("getDogPicturesBtn");
 let url = "https://dog.ceo/api/breed/hound/images";
 let box = document.getElementById("box");
+
 button.addEventListener("click", () => {
-    fetch(url).then(data => data.json())
-        .then((data) => {
-            if (data.status == "success") {
-                box.insertAdjacentElement("beforeend",
-                    `<img src="${data.message}">`);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "success") {
+                box.innerHTML += `<img src="${data.message}">`;
             } else {
-                alert("An error has occured!");
+                alert("An error has occurred!");
             }
-        }).catch(err => console.log(err));
-})
+        })
+        .catch(err => console.error(err));
+});
